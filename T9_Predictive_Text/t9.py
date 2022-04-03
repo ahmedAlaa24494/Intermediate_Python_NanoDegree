09467773117b9db4b6782e51ebd16dddc4ed4303
ahmedAlaa24494 <32624556+ahmedAlaa24494@users.noreply.github.com>
2022-04-03 09:44:36 +0200

    T9 Project

--- T9_Predictive_Text/t9.py
+++ T9_Predictive_Text/t9.py
@@ -2,9 +2,16 @@ import helper
 import gold
 
 def parse_content(content):
-    return {}
+    data = content.split('\n')
+    dataset = {}
+    for line in data : 
+        word , number = tuple(line.split())
+        dataset[word]  =number 
+    return dataset
+
 
 def make_tree(words):
+    #In this function we must create tree for each word , 
     return {}
 
 def predict(tree, numbers):
@@ -12,7 +19,8 @@ def predict(tree, numbers):
 
 
 if __name__ == '__main__':
-    content = helper.read_content(filename='ngrams-10k.txt')
+
+    content = helper.read_content(filename='Data/ngrams_10k.txt')
 
     # When you've finished implementing a part, remove the `gold.` prefix to check your own code.
 
@@ -22,18 +30,19 @@ if __name__ == '__main__':
     # PART 2: Building a trie from a collection of words.
     tree = gold.make_tree(words)
 
-    while True:
-        # PART 3: Predict words that could follow
-        numbers = helper.ask_for_numbers()
-        predictions = gold.predict(tree, numbers)
+    # while True:
+    #     # PART 3: Predict words that could follow
+    #     numbers = helper.ask_for_numbers()
+    #     predictions = gold.predict(tree, numbers)
 
-        if not predictions:
-            print('No words were found that match those numbers. :(')
-        else:
-            for prediction, frequency in predictions[:10]:
-                print(prediction, frequency)
+    #     if not predictions:
+    #         print('No words were found that match those numbers. :(')
+    #     else:
+    #         for prediction, frequency in predictions[:10]:
+    #             print(prediction, frequency)
 
-        response = input('Want to go again? [y/N] ')
-        again = response and response[0] in ('y', 'Y')
-        if not again:
-            break
+    #     response = input('Want to go again? [y/N] ')
+    #     again = response and response[0] in ('y', 'Y')
+    #     if not again:
+    #         break
+    print(words)
