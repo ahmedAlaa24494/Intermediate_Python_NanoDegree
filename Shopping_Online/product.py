a04cb1cc4b05fa29bbf9f4b1cf8f3eea79ea134b
ahmedAlaa24494 <32624556+ahmedAlaa24494@users.noreply.github.com>
2022-04-06 13:43:03 +0200

    Add __str__ method to product class to print the product information

--- Shopping_Online/product.py
+++ Shopping_Online/product.py
@@ -1,8 +1,11 @@
 class Product:
-    def __init__(self, name, description, price, seller, available=True):
+    def __init__(self, name, description, seller, price, available):
         self.name = name
         self.description = description
-        self.price = price
         self.seller = seller
-        self.available = True
         self.reviews = []
+        self.price = price
+        self.available = available
+
+    def __str__(self):
+        return f"Product({self.name}, {self.description}) at ${self.price}"
